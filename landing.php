dce6040e02e29853aa769a57fc535a86f92b42fc
Kritsanasak Kunraros <roboconk@gmail.com>
2020-07-22 09:20:05 +0700

    ปรับรูปหน้าแรก

--- landing.php
+++ landing.php
@@ -5,76 +5,44 @@
 <div class="row row-cols-1 row-cols-md-4">
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-1.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">โรคต้อหินคืออะไร</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma01.jpg" class="card-img-top mx-auto" alt="โรคต้อหินคืออะไร">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-2.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">ทราบได้อย่างไรว่าเป็นโรคต้อหิน</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma02.jpg" class="card-img-top mx-auto" alt="ทราบได้อย่างไรว่าเป็นโรคต้อหิน">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-3.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">อาการที่สงสัย</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
+      <img src="images/glaucoma03.jpg" class="card-img-top mx-auto" alt="อาการที่สงสัย">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-4.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">ปัจจัยเสี่ยงโรคต้อหิน</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma04.jpg" class="card-img-top mx-auto" alt="ปัจจัยเสี่ยงโรคต้อหิน">
     </div>
   </div>
 </div> 
 <div class="row row-cols-1 row-cols-md-4">
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-5.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">ระยะเวลาที่เป็นต้อหินกับอันตรายของโรค</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma05.jpg" class="card-img-top mx-auto" alt="ระยะเวลาที่เป็นต้อหินกับอันตรายของโรค">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-6.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">การรักษาโรคต้อหิน</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma06.jpg" class="card-img-top mx-auto" alt="การรักษาโรคต้อหิน">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-1.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">การปฏิบัติตัว</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
+      <img src="images/glaucoma07.jpg" class="card-img-top mx-auto" alt="การปฏิบัติตัว">
     </div>
   </div>
   <div class="col mb-3">
     <div class="card">
-      <img src="images/sample-pics/icon/icon-2.png" class="card-img-top mx-auto" style="width:160px;" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">ข้อควรรู้ในการใช้ยาหยอดตารักษาโรคต้อหิน</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+      <img src="images/glaucoma08.jpg" class="card-img-top mx-auto" alt="ข้อควรรู้ในการใช้ยาหยอดตารักษาโรคต้อหิน">
     </div>
   </div>
 </div>
